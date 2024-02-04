3e26dea814dd48510d52572ae8ac409e63b95fc3
Rail Aliiev <rail@iqchoice.com>
2024-02-04 14:50:03 -0500

    Add Noice.nvim

--- modules/home-manager/config/nvim/lua/user/init.lua
+++ modules/home-manager/config/nvim/lua/user/init.lua
@@ -37,6 +37,14 @@ local config = {
       event = "User AstroFile",
       opts = {},
     },
+    { "folke/noice.nvim",
+      event = "VeryLazy",
+      opts = {},
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+      },
+    },
   },
 
   polish = function()
