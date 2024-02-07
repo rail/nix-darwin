9c7a9242b26b0f158920f173262835987903262c
Rail Aliiev <rail@iqchoice.com>
2024-02-06 19:26:28 -0500

    Update last_position

--- modules/home-manager/config/nvim/lua/user/polish.lua
+++ modules/home-manager/config/nvim/lua/user/polish.lua
@@ -1,8 +1,12 @@
 remember_last_position = function()
   vim.api.nvim_create_autocmd(
-    { "BufReadPost" },
+    { "BufWinEnter" },
     {
-      callback = function()
+      callback = function(opts)
+        ft = vim.bo[opts.buf].filetype
+        if ft:match('commit') or ft:match('rebase') then
+         return
+        end
         local row, col = unpack(vim.api.nvim_buf_get_mark(0, "\""))
         if {row, col} ~= {0, 0} then
 				  vim.api.nvim_win_set_cursor(0, {row, col})
